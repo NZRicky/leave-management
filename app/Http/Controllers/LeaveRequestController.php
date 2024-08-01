d6d73a4a4cb6be8c7975bcdae16eaeab7f8edbb6
Ricky Li <nzrickyli@gmail.com>
2024-08-01 21:09:15 +0800

    feat: Use Artisan to generate LeaveRequest model
    This includes:
    - a model, migration, factory, seeder, policy, controller, and form requests.
    - generate a resource controller for API usage.

---
+++ app/Http/Controllers/LeaveRequestController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLeaveRequestRequest;
+use App\Http\Requests\UpdateLeaveRequestRequest;
+use App\Models\LeaveRequest;
+
+class LeaveRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLeaveRequestRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LeaveRequest $leaveRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLeaveRequestRequest $request, LeaveRequest $leaveRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LeaveRequest $leaveRequest)
+    {
+        //
+    }
+}
