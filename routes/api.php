55fd14104c93afe13e351b3379892e580359bc59
Ricky Li <nzrickyli@gmail.com>
2024-08-02 10:55:39 +0800

    feat: enable API routing by run artisan install:api

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\LeaveRequestController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('leave-requests', LeaveRequestController::class);
