1dbb83e739081fb348d836756daba83aef0eca8f
Ricky Li <nzrickyli@gmail.com>
2024-08-02 12:17:16 +0800

    TDD: write store leave request feature test

--- tests/Feature/LeaveRequestControllerTest.php
+++ tests/Feature/LeaveRequestControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\LeaveRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -21,11 +22,37 @@ class LeaveRequestControllerTest extends TestCase
     /**
      * test store method of the controller
      */
-    public function test_store_returns_a_successful_response(): void
+    public function test_can_store_new_leave_request(): void
     {
-        $response = $this->post('/api/leave-requests');
+        // get fake leave request
+        $leaveRequest = LeaveRequest::factory()->make();
 
-        $response->assertStatus(200);
+        $response = $this->postJson('/api/leave-requests', [
+            'user_id' => $leaveRequest->user_id,
+            'start_date' => $leaveRequest->start_date,
+            'end_date' => $leaveRequest->end_date,
+            'leave_type' => $leaveRequest->leave_type,
+            'reason' => $leaveRequest->reason
+        ]);
+
+        $response->assertStatus(201)
+            ->assertJson([
+                'data' => [
+                    'user_id' => $leaveRequest->user_id,
+                    'start_date' => $leaveRequest->start_date,
+                    'end_date' => $leaveRequest->end_date,
+                    'leave_type' => $leaveRequest->leave_type,
+                    'reason' => $leaveRequest->reason
+                ]
+            ]);
+
+        $this->assertDatabaseHas('leave_requests', [
+            'user_id' => $leaveRequest->user_id,
+            'start_date' => $leaveRequest->start_date,
+            'end_date' => $leaveRequest->end_date,
+            'leave_type' => $leaveRequest->leave_type,
+            'reason' => $leaveRequest->reason
+        ]);
 
     }
 
