558f77cf5b41fe5a562613c7de2d869878dbcc5b
Ricky Li <nzrickyli@gmail.com>
2024-08-02 11:11:45 +0800

    generate leave request resource class

---
+++ app/Http/Resources/LeaveRequestResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LeaveRequestResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'reason' => $this->reason,
+            'leave_type' => $this->leave_type
+        ];
+    }
+}
