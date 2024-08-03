572f9b7dc3f29a031a7fc439413e4e3dea61b3c4
Ricky Li <nzrickyli@gmail.com>
2024-08-03 14:04:17 +0800

    Use date time picker and style it

--- frontend/src/Pages/LeaveRequests/Create.jsx
+++ frontend/src/Pages/LeaveRequests/Create.jsx
@@ -1,14 +1,36 @@
 
+import { useState } from "react";
+import DateTimePicker from "react-datetime-picker";
+import "react-datetime-picker/dist/DateTimePicker.css";
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
+
 export default function Create() {
+
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
 	return (
 		<div className="max-w-md mx-auto bg-white p-8 rounded-md shadow-md">
 			<h2 className="text-2xl font-bold mb-6">New Leave Request</h2>
 			<form>
 				<div className="mb-4">
 					<label htmlFor="name" className="block text-sm font-medium text-gray-700 mb-1">Start Date</label>
+					<DateTimePicker
+						id="start-date"
+						value={startDate}
+						onChange={setStartDate}
+						className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+					></DateTimePicker>
 				</div>
 				<div className="mb-4">
 					<label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">End Date</label>
+					<DateTimePicker
+						id="end-date"
+						value={endDate}
+						onChange={setEndDate}
+						className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+					></DateTimePicker>					
 				</div>
 
 				<div className="mb-4">
@@ -42,7 +64,7 @@ export default function Create() {
 						rows="4"
 						className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
 					></textarea>
-				</div>								
+				</div>
 				<div>
 					<button
 						type="submit"
