b37b76899128116ecaa65c840ea9e8ac3b10d18e
Ricky Li <nzrickyli@gmail.com>
2024-08-03 16:07:53 +0800

    feat: add form errors to each field
    This includes:
    - show error if the start date is before the current date.
    - show error if the end date is before the start date.
    - show error if the reason is empty.
    - show error if the leave type is empty.
    - show error if user is empty.

--- frontend/src/Pages/LeaveRequests/Create.jsx
+++ frontend/src/Pages/LeaveRequests/Create.jsx
@@ -9,16 +9,59 @@ import { Link } from "react-router-dom";
 
 export default function Create() {
 
-	const [startDate, setStartDate] = useState(new Date());
-	const [endDate, setEndDate] = useState(new Date());
+	const [startDate, setStartDate] = useState('');
+	const [endDate, setEndDate] = useState('');
 
 	const [userId, setUserId] = useState(0);
 	const [reason, setReason] = useState('');
 	const [leaveType, setLeaveType] = useState('');
 
+	const [errors, setErrors] = useState({});
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
+		const formErrors = {};
+
+		if (!startDate) {
+			formErrors.startDate = 'Please select a start date';
+		}
+
+		// check if start date before current date
+		if (new Date() > new Date(startDate)) {
+            formErrors.startDate = 'Start date cannot be before current date';
+        }		
+		
+		if (!endDate) {
+			formErrors.endDate = 'Please select an end date';
+		}
+
+		// check if end date before start date
+		if (startDate && endDate && new Date(endDate) < new Date(startDate)) {
+            formErrors.endDate = 'End date cannot be before start date';
+        }
+
+		if (!leaveType) {
+			formErrors.leaveType = 'Please select leave type';
+		}
+		
+		if (!userId) {
+			formErrors.userId = 'Please select a user';
+		}
+
+		if (!reason) {
+			formErrors.reason = 'Please provide a reason';
+		}
+
+		setErrors(formErrors);
+
+		if (Object.keys(formErrors).length !== 0) {
+			return false;
+		}
+
+		console.log(startDate, endDate, userId, leaveType, reason);
+
+		// no form errors, backend to process the form
 		try {
 			await axios.post("http://127.0.0.1:8000/api/leave-requests", {
 				startDate,
@@ -42,29 +85,32 @@ export default function Create() {
 			<h2 className="text-2xl font-bold mb-6">New Leave Request</h2>
 			<form onSubmit={handleSubmit}>
 				<div className="mb-4">
-					<label htmlFor="name" className="block text-sm font-medium text-gray-700 mb-1">Start Date</label>
+					<label htmlFor="start-date" className="block text-sm font-medium text-gray-700 mb-1">Start Date</label>
 					<DateTimePicker
 						id="start-date"
 						value={startDate}
 						onChange={setStartDate}
 						className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
 					></DateTimePicker>
+					{errors.startDate && <p className="text-red-500 text-sm mt-1">{errors.startDate}</p>}
 				</div>
 				<div className="mb-4">
-					<label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">End Date</label>
+					<label htmlFor="end-date" className="block text-sm font-medium text-gray-700 mb-1">End Date</label>
 					<DateTimePicker
 						id="end-date"
 						value={endDate}
 						onChange={setEndDate}
 						className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
 					></DateTimePicker>
+					{errors.endDate && <p className="text-red-500 text-sm mt-1">{errors.endDate}</p>}
 				</div>
 
 				<div className="mb-4">
 					<label htmlFor="leave-type" className="block text-sm font-medium text-gray-700 mb-1">Leave Type</label>
 					<select
-						id="leave-type"
-						onChange={setLeaveType}
+						id="leave-type" 
+						value={leaveType} 
+						onChange={(e) => setLeaveType(e.target.value)}
 						className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
 					>
 						<option value="">Please select</option>
@@ -73,29 +119,33 @@ export default function Create() {
 						<option value="vacation">Vacation</option>
 						<option value="bereavement">Bereavement</option>
 					</select>
+					{errors.leaveType && <p className="text-red-500 text-sm mt-1">{errors.leaveType}</p>}
 				</div>
 				<div className="mb-4">
 					<label htmlFor="user" className="block text-sm font-medium text-gray-700 mb-1">User</label>
 					<select
-						id="user"
-						onChange={setUserId}
+						id="user" 
+						value={userId} 
+						onChange={(e) => setUserId(e.target.value)}
 						className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
 					>
 						<option value="">Please select</option>
 						<option value="1">Ricky</option>
 						<option value="2">Susan</option>
 						<option value="3">Richard</option>
-
 					</select>
+					{errors.userId && <p className="text-red-500 text-sm mt-1">{errors.userId}</p>}
 				</div>
 				<div className="mb-4">
 					<label htmlFor="reason" className="block text-sm font-medium text-gray-700 mb-1">Reason</label>
-					<textarea
+					<textarea 
+						maxLength={50}
 						id="reason"
 						onChange={(e) => setReason(e.target.value)}
 						rows="4"
 						className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
 					></textarea>
+					{errors.reason && <p className="text-red-500 text-sm mt-1">{errors.reason}</p>}
 				</div>
 				<div>
 					<Link
