1e659bdc35282e41d661981479f7345261918734
Ricky Li <nzrickyli@gmail.com>
2024-08-03 10:50:24 +0800

    feat: add create new leave request button
    This includes:
    - new router /leave-requests/create
    - new Create component
    - style create button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Home from "./Pages/Home";
+import { Route, Routes } from 'react-router-dom';
+import Create from './Pages/LeaveRequests/Create';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="leave-requests/create" element={<Create />} />
+      </Routes>
     </>
   )
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Home() {
 
 	const [leaveRequests, setLeaveRequests] = useState([]);
-	
+
 	// calculate leave days with 2 decimal place
 	const calculateLeaveDays = (startDate, endDate) => {
 		const start = new Date(startDate);
@@ -27,30 +28,38 @@ export default function Home() {
 
 
 	return (
-		<div className="table-wrapper">
-			<table className="min-w-full divide-y divide-gray-200">
-				<thead className="bg-gray-100">
-					<tr>
-						<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">User</th>
-						<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Start Date</th>
-						<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">End Date</th>
-						<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">No. of Days</th>
-						<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Leave Type</th>
-					</tr>
-				</thead>
-				<tbody className="bg-white divide-y divide-gray-200">
-					{leaveRequests && leaveRequests.map((leaveRequest) => (
-						<tr className="odd:bg-gray-50 even:bg-white" key={leaveRequest.id}>
-							<td className="px-6 py-2 whitespace-nowrap">{leaveRequest.user_id}</td>
-							<td className="px-6 py-2 whitespace-nowrap">{leaveRequest.start_date}</td>
-							<td className="px-6 py-2 whitespace-nowrap">{leaveRequest.end_date}</td>
-							<td className="px-6 py-2 whitespace-nowrap">{calculateLeaveDays(leaveRequest.start_date, leaveRequest.end_date)}</td>
-							<td className="px-6 py-2 whitespace-nowrap">{leaveRequest.leave_type}</td>
+		<div>
+			<Link
+				to="/leave-requests/create"
+				className="px-4 py-2 mt-4 rounded-md bg-blue-500 text-white float-right mb-10"
+			>
+				Create new Leave Request
+			</Link>
+			<div className="table-wrapper">
+				<table className="min-w-full divide-y divide-gray-200">
+					<thead className="bg-gray-100">
+						<tr>
+							<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">User</th>
+							<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Start Date</th>
+							<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">End Date</th>
+							<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">No. of Days</th>
+							<th className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider">Leave Type</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
+					</thead>
+					<tbody className="bg-white divide-y divide-gray-200">
+						{leaveRequests && leaveRequests.map((leaveRequest) => (
+							<tr className="odd:bg-gray-50 even:bg-white" key={leaveRequest.id}>
+								<td className="px-6 py-2 whitespace-nowrap">{leaveRequest.user_id}</td>
+								<td className="px-6 py-2 whitespace-nowrap">{leaveRequest.start_date}</td>
+								<td className="px-6 py-2 whitespace-nowrap">{leaveRequest.end_date}</td>
+								<td className="px-6 py-2 whitespace-nowrap">{calculateLeaveDays(leaveRequest.start_date, leaveRequest.end_date)}</td>
+								<td className="px-6 py-2 whitespace-nowrap">{leaveRequest.leave_type}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 
+			</div>
 		</div>
 	);
 }
